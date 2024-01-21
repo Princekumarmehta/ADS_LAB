83238a09939f823c1c034b8f7ce88ce8334e6ce0
Prince Kumar <princekumar.is21@bmce.ac.in>
2024-01-21 18:26:13 +0530

    Added Rabin karp

--- rabin_karp.cpp
+++ rabin_karp.cpp
@@ -1,7 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int q = 256;
+
+int calculate_hash(string s)
+{
+    int score = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        score = (score * q + s[i]) % INT_MAX;
+    }
+    return score;
+}
+
+int rabinKarp(string s, string p)
+{
+    int p_score = calculate_hash(p);
+    int n = p.size();
+    int temp = 0;
+    for (int i = 0; i <= s.size() - n; i++)
+    {
+        if (s[i] == p[0])
+        {
+            temp = calculate_hash(s.substr(i, n));
+            if (temp == p_score)
+                return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
+    string s1, s2;
+    cout << "Enter string: \n";
+    cin >> s1;
+    cout << "Enter pattern to be searched in string: \n";
+    cin >> s2;
+    int index = rabinKarp(s1, s2);
+    if (index == -1)
+        cout << "Pattern is not present in String ";
+    else
+    {
+        cout << "Pattern is found at index " << index << ", after " << index + 1 << " iteration(s)";
+    }
     return 0;
 }
